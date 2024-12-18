17df828e36cfc6827541e35a26c7a9b961263e7f
Aryan Kanojia <122448915+Aryankanojia9654@users.noreply.github.com>
2024-12-18 23:49:02 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (22.63%) - LeetSync

--- LinkedList/206-reverse-linked-list/reverse-linked-list.cpp
+++ LinkedList/206-reverse-linked-list/reverse-linked-list.cpp
@@ -12,41 +12,59 @@ class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
 
+        //bruteforce
+
         if(head == NULL || head->next == NULL){
             return head;
 
         }
-        vector<int>vec;
-        ListNode * temp = head;
+        // vector<int>vec;
+        // ListNode * temp = head;
 
 
-        while(temp){
-            vec.push_back(temp->val);
-            temp = temp->next;
+        // while(temp){
+        //     vec.push_back(temp->val);
+        //     temp = temp->next;
+        // }
+
+
+        // reverse(vec.begin() , vec.end());
+
+        // head = NULL;
+
+        // if(head == NULL){
+        //     head = new ListNode(vec[0]);
+        // }
+
+        // temp = head;
+
+
+        // for(int i = 1 ; i<vec.size() ; i++){
+        //     ListNode *temp1 = new ListNode(vec[i]);
+        //     temp->next = temp1;
+        //     temp = temp1;
+
+        // }
+
+        // return head;
+
+        // recursive
+
+ ListNode *prev = NULL;
+        ListNode *curr = head;
+        ListNode *front = NULL; // Make sure front is initialized to NULL.
+
+        while(curr != NULL) {
+            front = curr->next;  // Store the next node before changing pointers
+            curr->next = prev;   // Reverse the current node's pointer
+            prev = curr;         // Move prev to current
+            curr = front;        // Move to the next node
         }
 
-
-        reverse(vec.begin() , vec.end());
-
-        head = NULL;
-
-        if(head == NULL){
-            head = new ListNode(vec[0]);
-        }
-
-        temp = head;
-
-
-        for(int i = 1 ; i<vec.size() ; i++){
-            ListNode *temp1 = new ListNode(vec[i]);
-            temp->next = temp1;
-            temp = temp1;
-
-        }
-
-        return head;
+        return prev;  // prev will be the new head of the reversed list
+    }
 
         
 
-    }
+    
 };
