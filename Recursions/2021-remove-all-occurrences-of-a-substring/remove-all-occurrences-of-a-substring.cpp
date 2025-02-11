92a7ef3763c445e1436f0af604c965ec1d3849c0
Aryan Kanojia <122448915+Aryankanojia9654@users.noreply.github.com>
2025-02-12 02:00:54 +0530

    Time: 4 ms (15.12%) | Memory: 12.2 MB (8.36%) - LeetSync

---
+++ Recursions/2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        string result = ""; 
+        
+        for (char c : s) {
+            result.push_back(c); 
+            
+            if (result.size() >= part.size() && result.substr(result.size() - part.size()) == part) {
+                result.erase(result.size() - part.size());
+            }
+        }
+        
+        return result; 
+    }
+};
