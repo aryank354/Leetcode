66403894b91bd94b4f58d606363326e782f4ed12
Aryan Kanojia <122448915+aryankanojia354@users.noreply.github.com>
2025-03-14 17:58:19 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (23.40%) - LeetSync

--- Recursions/2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
+++ Recursions/2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
@@ -3,7 +3,6 @@ public:
     vector<int> applyOperations(vector<int>& nums) {
         int n = nums.size();
         
-        // Step 1: Merge adjacent equal numbers
         for (int i = 0; i < n - 1; i++) {
             if (nums[i] == nums[i + 1]) {
                 nums[i] *= 2;
@@ -11,7 +10,6 @@ public:
             }
         }
 
-        // Step 2: Shift non-zero elements to the left
         int insertPos = 0;
         for (int i = 0; i < n; i++) {
             if (nums[i] != 0) {
