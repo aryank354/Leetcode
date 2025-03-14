77a0e30d3f2bfa5bae505b795a6cc0b515d49cf0
Aryan Kanojia <122448915+aryankanojia354@users.noreply.github.com>
2025-03-14 18:09:58 +0530

    Time: 1 ms (13.65%) | Memory: 13.1 MB (61.15%) - LeetSync

--- Recursions/2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
+++ Recursions/2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
@@ -1,27 +1,64 @@
+//Approach-1 (2 Pass)
+//T.C : O(n) 
+//S.C : O(1)
 class Solution {
 public:
     vector<int> applyOperations(vector<int>& nums) {
         int n = nums.size();
-        
-        for (int i = 0; i < n - 1; i++) {
-            if (nums[i] == nums[i + 1]) {
+
+        //Simulation
+        for(int i = 0; i < n-1; i++) {
+            if(nums[i] == nums[i+1] && nums[i] != 0) {
                 nums[i] *= 2;
-                nums[i + 1] = 0;
+                nums[i+1] = 0;
             }
         }
 
-        int insertPos = 0;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] != 0) {
-                nums[insertPos++] = nums[i];
+        int j = 0;
+        for(int i = 0; i < n; i++) {
+            if(nums[i] != 0) {
+                nums[j] = nums[i];
+                j++;
             }
         }
 
-        // Step 3: Fill the remaining positions with zeros
-        while (insertPos < n) {
-            nums[insertPos++] = 0;
+        while(j < n) {
+            nums[j] = 0;
+            j++;
         }
 
         return nums;
+
     }
 };
+
+
+
+// //Approach-2 (1 Pass)
+// //T.C : O(n) 
+// //S.C : O(1)
+// class Solution {
+// public:
+//     vector<int> applyOperations(vector<int>& nums) {
+//         int n = nums.size();
+
+//         //Simulation
+//         int j = 0;
+//         for(int i = 0; i < n; i++) {
+//             if(i+1 < n && nums[i] == nums[i+1] && nums[i] != 0) {
+//                 nums[i] *= 2;
+//                 nums[i+1] = 0;
+//             }
+
+//             if(nums[i] != 0) {
+//                 if(i != j) {
+//                     swap(nums[i], nums[j]);
+//                 }
+//                 j++;
+//             }
+//         }
+
+//         return nums;
+
+//     }
+// };
