8c6c4d636fc2a9899b934ced25f640bda0519f39
Aryan Kanojia <122448915+aryankanojia354@users.noreply.github.com>
2025-03-23 02:43:08 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (57.51%) - LeetSync

---
+++ Recursions/8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    
+void helperAtoi(int n, const string &s, long long &ans, bool is_negative) {
+    if (n >= s.size() || !isdigit(s[n])) return;
+    
+    int digit = s[n] - '0';
+    
+    if (!is_negative) {
+        if (ans > (long long)INT_MAX / 10 ||
+           (ans == (long long)INT_MAX / 10 && digit > 7)) {
+            ans = INT_MAX;
+            return;
+        }
+    } else {
+        if (ans > (long long)INT_MAX / 10 ||
+           (ans == (long long)INT_MAX / 10 && digit > 8)) {
+            ans = (long long)INT_MAX + 1;  
+            return;
+        }
+    }
+    
+    ans = ans * 10 + digit;
+    helperAtoi(n + 1, s, ans, is_negative);
+}
+
+    int myAtoi(string s) {
+    int n = 0;
+    long long ans = 0;
+    bool is_negative = false;
+
+    while(n < s.size() && s[n] == ' ') {
+        n++;
+    }
+    
+    if(n < s.size() && (s[n] == '-' || s[n] == '+')) {
+        is_negative = (s[n] == '-');
+        n++;
+    }
+    
+    helperAtoi(n, s, ans, is_negative);
+    
+    if(is_negative) {
+        if(ans > (long long)INT_MAX + 1) {
+            return INT_MIN;
+        }
+        return -ans;
+    } else {
+        if(ans > INT_MAX) {
+            return INT_MAX;
+        }
+        return ans;
+    }
+}
+
+};
