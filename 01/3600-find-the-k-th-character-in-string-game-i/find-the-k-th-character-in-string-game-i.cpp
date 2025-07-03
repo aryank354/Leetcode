df778b59bfa0f1cafa24792081a3416659111a3b
Aryan Kanojia <122448915+aryankanojia354@users.noreply.github.com>
2025-07-04 02:51:18 +0530

    Time: 4 ms (36.05%) | Memory: 10.9 MB (17.36%) - LeetSync

---
+++ 01/3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.cpp
@@ -0,0 +1,55 @@
+// class Solution {
+// public:
+    
+//     char func(int k , string op){
+//         if(op.length() ==  k){
+//             return ' ';
+//         }
+
+//         string op1 = op;
+//         int s = op.length();
+//         for(int i = 0 ; i<s ; i++){
+//             if(op[i] == 'z'){
+//                 op.push_back('a');
+//                 continue;
+//             }
+//             op.push_back(op[i]+1);
+//         }
+
+//         func(k , op1);
+
+//         return op[k-1];
+//     }
+//     char kthCharacter(int k) {
+//         string op = "";
+
+//         return func(k , op);
+//     }
+// };
+
+class Solution {
+public:
+
+    char func(int k , string op) {
+        if(op.length() >= k) {
+            return op[k - 1];
+        }
+
+        string op1 = op;
+        int s = op.length();
+        for(int i = 0 ; i < s ; i++) {
+            if(op[i] == 'z') {
+                op.push_back('a');
+            } else {
+                op.push_back(op[i] + 1);
+            }
+        }
+
+        return func(k , op);
+    }
+
+    char kthCharacter(int k) {
+        string op = "a";
+        return func(k , op);
+    }
+};
