8886d4f67119639498afbbf22d87319306a816d0
Aryan Kanojia <122448915+aryank354@users.noreply.github.com>
2025-07-29 23:05:18 +0530

    Time: 18 ms (97.50%) | Memory: 19.4 MB (41.51%) - LeetSync

--- 01/200-number-of-islands/number-of-islands.cpp
+++ 01/200-number-of-islands/number-of-islands.cpp
@@ -1,48 +1,48 @@
 class Solution {
 public:
-    void bfs(int x, int y, vector<vector<int>> &vis, vector<vector<char>>& grid, int &count) {
-        count++;
-        int n = grid.size();
-        int m = grid[0].size();
-        queue<pair<int, int>> q;
-        q.push({x, y});
-        vis[x][y] = 1;
 
-        int dx[4] = {-1, 0, 1, 0};
-        int dy[4] = {0, 1, 0, -1};
 
-        while (!q.empty()) {
-            int nodex = q.front().first;
-            int nodey = q.front().second;
-            q.pop();
 
-            for (int k = 0; k < 4; ++k) {
-                int delx = nodex + dx[k];
-                int dely = nodey + dy[k];
+int numIslands(vector<vector<char>>& grid) {
+    int n = grid.size();
+    int m = grid[0].size();
+    int count = 0;
+    vector<vector<int>>vis(n , vector<int>(m,0));
+    queue<pair<int,int>>que;
 
-                if (delx >= 0 && delx < n && dely >= 0 && dely < m &&
-                    grid[delx][dely] == '1' && vis[delx][dely] == 0) {
-                    q.push({delx, dely});
-                    vis[delx][dely] = 1;
+    int dx[4] = { -1 , 0 , 1 , 0};
+    int dy[4] = { 0 , 1, 0 , -1};
+
+    for(int i =0 ; i<n ; i++){
+        for(int j = 0 ; j<m ; j++){
+            if(vis[i][j] == 0 && grid[i][j] == '1'){
+                count++;
+                que.push({i,j});
+                vis[i][j] = 1;
+
+                while(!que.empty()){
+                    int x = que.front().first;
+                    int y = que.front().second;
+                    que.pop();
+
+                    
+
+                    for(int i = 0 ; i<4 ; i++){
+                        int newx = x + dx[i];
+                        int newy = y + dy[i];
+
+                        if(newx >= 0 && newx <n && newy >=0 && newy < m && vis[newx][newy] == 0 && grid[newx][newy] == '1'){
+                            vis[newx][newy] = 1;
+                            que.push({newx , newy});
+                        }
+
+                    }
                 }
             }
         }
     }
 
-    int numIslands(vector<vector<char>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        int count = 0;
-        vector<vector<int>> vis(n, vector<int>(m, 0));
+    return count;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (vis[i][j] == 0 && grid[i][j] == '1') {
-                    bfs(i, j, vis, grid, count);
-                }
-            }
-        }
-
-        return count;
     }
 };
