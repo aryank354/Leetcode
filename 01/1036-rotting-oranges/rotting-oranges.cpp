ec00212d834729faa8c603a2a45102db07ef8033
Aryan Kanojia <122448915+aryank354@users.noreply.github.com>
2025-07-30 02:44:25 +0530

    Time: 11 ms (6.89%) | Memory: 17.1 MB (47.09%) - LeetSync

--- 01/1036-rotting-oranges/rotting-oranges.cpp
+++ 01/1036-rotting-oranges/rotting-oranges.cpp
@@ -1,42 +1,51 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& grid, int i, int j, int time) {
-        int n = grid.size();
-        int m = grid[0].size();
-
-        // If out of bounds or cell is empty or already has lesser/equal time rot
-        if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] == 0) return;
-        if (grid[i][j] > 1 && grid[i][j] < time) return;
-
-        grid[i][j] = time; // Update rot time
-
-        dfs(grid, i + 1, j, time + 1);
-        dfs(grid, i - 1, j, time + 1);
-        dfs(grid, i, j + 1, time + 1);
-        dfs(grid, i, j - 1, time + 1);
-    }
-
     int orangesRotting(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
 
-        // Start DFS from all initially rotten oranges
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < m; ++j) {
+        // This will store the minimum time at which each cell gets rotten
+        vector<vector<int>> time(n, vector<int>(m, INT_MAX));
+
+        // Direction vectors: up, right, down, left
+        int dx[4] = {-1, 0, 1, 0};
+        int dy[4] = {0, 1, 0, -1};
+
+        // DFS function
+        function<void(int, int, int)> dfs = [&](int x, int y, int t) {
+            if (x < 0 || x >= n || y < 0 || y >= m) return;
+            if (grid[x][y] == 0 || time[x][y] <= t) return;  // wall or already better time
+
+            time[x][y] = t;
+
+            for (int i = 0; i < 4; i++) {
+                int newX = x + dx[i];
+                int newY = y + dy[i];
+                dfs(newX, newY, t + 1);
+            }
+        };
+
+        // Run DFS from each rotten orange (value == 2)
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 if (grid[i][j] == 2) {
-                    dfs(grid, i, j, 2); // Start time from 2 to distinguish from fresh
+                    dfs(i, j, 0);
                 }
             }
         }
 
-        int maxTime = 2;
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < m; ++j) {
-                if (grid[i][j] == 1) return -1;  // Fresh orange remains
-                maxTime = max(maxTime, grid[i][j]);
+        int maxTime = 0;
+
+        // Now, check each fresh orange
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1) {
+                    if (time[i][j] == INT_MAX) return -1; // never rotted
+                    maxTime = max(maxTime, time[i][j]);
+                }
             }
         }
 
-        return maxTime - 2; // Subtract initial offset
+        return maxTime;
     }
 };
