36c25c7aab2b7488119650fe4999ff73ad500b64
Aryan Kanojia <122448915+aryank354@users.noreply.github.com>
2025-07-30 01:10:03 +0530

    Time: 21 ms (91.55%) | Memory: 17.4 MB (55.31%) - LeetSync

--- 01/200-number-of-islands/number-of-islands.cpp
+++ 01/200-number-of-islands/number-of-islands.cpp
@@ -2,42 +2,36 @@ class Solution {
 public:
 
 
+void dfs(int x , int y , vector<vector<char>>&grid , vector<vector<int>>&vis , int n , int m){
+    
+    vis[x][y] = 1;
+    int dx[4] = { -1 , 0 , 1 , 0};
+    int dy[4] = {0 , 1 , 0 , -1};
+
+    for(int i =0 ; i<4 ; i++){
+        int delx = x + dx[i];
+        int dely = y + dy[i];
+
+        if(delx >= 0 && delx <n && dely >= 0 && dely <m && vis[delx][dely] == 0 && grid[delx][dely] == '1'){
+            vis[delx][dely] = 1;
+            dfs(delx , dely , grid , vis , n , m);
+        }
+    }
+}
+
+
 
 int numIslands(vector<vector<char>>& grid) {
     int n = grid.size();
     int m = grid[0].size();
-    int count = 0;
     vector<vector<int>>vis(n , vector<int>(m,0));
-    queue<pair<int,int>>que;
+    int count = 0;
 
-    int dx[4] = { -1 , 0 , 1 , 0};
-    int dy[4] = { 0 , 1, 0 , -1};
-
-    for(int i =0 ; i<n ; i++){
+    for(int i = 0 ; i<n ; i++){
         for(int j = 0 ; j<m ; j++){
             if(vis[i][j] == 0 && grid[i][j] == '1'){
                 count++;
-                que.push({i,j});
-                vis[i][j] = 1;
-
-                while(!que.empty()){
-                    int x = que.front().first;
-                    int y = que.front().second;
-                    que.pop();
-
-                    
-
-                    for(int i = 0 ; i<4 ; i++){
-                        int newx = x + dx[i];
-                        int newy = y + dy[i];
-
-                        if(newx >= 0 && newx <n && newy >=0 && newy < m && vis[newx][newy] == 0 && grid[newx][newy] == '1'){
-                            vis[newx][newy] = 1;
-                            que.push({newx , newy});
-                        }
-
-                    }
-                }
+                dfs(i , j , grid , vis , n , m);
             }
         }
     }
